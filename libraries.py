54553e149069395ef28925216904bb0db7b5199b
Rajinikanth Kakarla <rajinikanthkakarla.03@gmail.com>
2023-10-04 08:03:26 +0530

    Delete libraries.py

--- libraries.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from transformers import pipeline
-from youtube_transcript_api import YouTubeTranscriptApi
-import re
-from deep_translator import GoogleTranslator
-import urllib.parse
